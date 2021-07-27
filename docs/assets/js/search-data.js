8f8f122e1290d06ea4875f293f97b8f55bc602a4
Marc Torres <mtorres.form@gmail.com>
2021-07-27 12:52:34 +0200

    Push the post
    THere is a strange issue with some files being forcefully deleted idk why

--- docs/assets/js/search-data.js
+++ docs/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Getting this blog setup",
-        "excerpt":"Hello test post Maybe the post had no words and that was the issue ","categories": ["Web"],
+        "excerpt":"Before anything else For this you will need any text editor and just knowing that terminal exist, where you write things and things go boom or vroom. My choice is Visual Studio Code which has built-in terminal support, is cross-platform and quite lightweight. Jekyll The framework used for setting up...","categories": ["Web"],
         "tags": ["markdown","html","jekyll"],
         "url": "/web/2021/07/21/Setup.html"
       },]
