c208b4f6f9b6cff5a4bcbdb375d828a51d92e74c
Marc Torres <mtorres.form@gmail.com>
2021-07-27 14:24:34 +0200

    Minor revision
    Should have waited to tweet it out

--- docs/assets/js/search-data.js
+++ docs/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Getting this blog setup",
-        "excerpt":"This post will be an overview on getting a simple Jekyll based blog working. For this you will need any text editor and just knowing that terminal exist, where you write things and things go boom or vroom. My choice is Visual Studio Code which has built-in terminal support, is...","categories": ["Web"],
+        "excerpt":"This post will be an overview on getting a simple Jekyll based blog working. For this you will need any text editor and just knowing that the terminal exist, where you write words and things go boom or vroom. My choice is Visual Studio Code which has built-in terminal support,...","categories": ["Web"],
         "tags": ["markdown","html","jekyll"],
         "url": "/web/2021/07/21/Setup.html"
       },]
