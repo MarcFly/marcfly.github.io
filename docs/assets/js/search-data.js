d731f15fddcde32e8cc2813bee814b8dbdb5aef6
Marc Torres <mtorres.form@gmail.com>
2021-07-27 13:12:06 +0200

    Issue with a misplaced tilde
    Also I don't understand really how the sitemap/feed plugins work but it bring maybe too many changes I don't exactly appreciate.
    
    For now it is working as I intend, have to revise probably ortography a bit more because I am careless but that will be done separately.

--- docs/assets/js/search-data.js
+++ docs/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Setup",
-        "excerpt":"`—title: “Getting this blog setup”description: How to make a Jekyll Blog easily and hassle-free.categories: Webtags: markdown html jekylllast_modified_at: 2021-07-22T11:00:00-01:00—This post will be an overview on getting a simple Jekyll based blog working. For this you will need any text editor and just knowing that terminal exist, where you write things...","categories": [],
-        "tags": [],
-        "url": "/2021/07/21/Setup.html"
+        "title": "Getting this blog setup",
+        "excerpt":"This post will be an overview on getting a simple Jekyll based blog working. For this you will need any text editor and just knowing that terminal exist, where you write things and things go boom or vroom. My choice is Visual Studio Code which has built-in terminal support, is...","categories": ["Web"],
+        "tags": ["markdown","html","jekyll"],
+        "url": "/web/2021/07/21/Setup.html"
       },]
